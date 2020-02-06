a756761d4dd6e7bc9a5dc3575a164452ae2ee13a
Gabnerd <ShaickMiles@gmail.com>
2020-02-06 20:43:43 -0300

    Update Administrador.java

--- src/java/br/com/direstock/model/Administrador.java
+++ src/java/br/com/direstock/model/Administrador.java
@@ -11,4 +11,63 @@ package br.com.direstock.model;
  */
 public class Administrador {
     
+    private int id;
+    private String nomeCompleto;
+    private String cpf;
+    private String rg;
+    private String funcaoEmpresa;
+
+    public Administrador() {
+    }
+
+    public Administrador(int id, String nomeCompleto, String cpf, String rg, String funcaoEmpresa) {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.funcaoEmpresa = funcaoEmpresa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getFuncaoEmpresa() {
+        return funcaoEmpresa;
+    }
+
+    public void setFuncaoEmpresa(String funcaoEmpresa) {
+        this.funcaoEmpresa = funcaoEmpresa;
+    }
+    
+    
+    
 }
